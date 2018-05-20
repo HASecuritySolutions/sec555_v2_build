8ed16c461c080b60f8fc4d62e1de86d0bdf295c1
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-20 15:15:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,9 +57,9 @@ sudo docker pull docker.elastic.co/beats/filebeat:6.2.4
 sudo docker pull hasecuritysolutions/logstashoss
 
 sudo chmod 755 /labs/1.1/filebeat.yml
-alias filebeat="docker run -it --rm --net=bridge -v /labs:/labs:ro -v /var/log:/var/log:ro --link logstash docker.elastic.co/beats/filebeat:6.2.4 /usr/share/filebeat/filebeat"
-alias logstash="docker run -it --rm --net=bridge --name logstash -v /labs:/labs --link elasticsearch -e ELASTICSEARCH_HOST=elasticsearch -p 5044:5044 -p 5045:5045 -p 6000:6000 -p 6050:6050 hasecuritysolutions/logstashoss /usr/share/logstash/bin/logstash"
+alias filebeat="docker run -it --rm --net=bridge --name filebeat --hostname filebeat -v /labs:/labs:ro -v /var/log:/var/log:ro --link logstash docker.elastic.co/beats/filebeat:6.2.4 /usr/share/filebeat/filebeat"
+alias logstash="docker run -it --rm --net=bridge --name logstash --hostname logstash -v /labs:/labs --link elasticsearch -e ELASTICSEARCH_HOST=elasticsearch -p 5044:5044 -p 5045:5045 -p 6000:6000 -p 6050:6050 hasecuritysolutions/logstashoss /usr/share/logstash/bin/logstash"
 alias pwsh="docker run -it -v /labs:/labs -v /scripts:/scripts -v /var/www:/var/www -v /home/student:/home/student -v /var/run/docker.sock:/var/run/docker.sock --rm --link elasticsearch hasecuritysolutions/wikiup /usr/bin/pwsh"
-alias curator="docker run -it -v /etc/localtime:/etc/localtime:ro -v /labs/curator:/labs/curator:ro -e TZ=America/Chicago -e ELASTICSEARCH_HOST=elasticsearch --net=bridge --rm --link elasticsearch hasecuritysolutions/elastic_cron /usr/local/bin/curator"
-alias elastalert="docker run -it --rm --net=bridge -v /labs/elastalert:/labs/elastalert --link elasticsearch hasecuritysolutions/elastalert /usr/local/bin/elastalert"
+alias curator="docker run -it -v /etc/localtime:/etc/localtime:ro --name curator --hostname curator -v /labs/curator:/labs/curator:ro -e TZ=America/Chicago -e ELASTICSEARCH_HOST=elasticsearch --net=bridge --rm --link elasticsearch hasecuritysolutions/elastic_cron /usr/local/bin/curator"
+alias elastalert="docker run -it --rm --net=bridge --name elastalert --hostname elastalert -v /labs/elastalert:/labs/elastalert --link elasticsearch hasecuritysolutions/elastalert /usr/local/bin/elastalert"
 alias wikiup="docker run -it --rm --net=bridge -v /labs:/labs -v /var/www/sec555-wiki:/var/www/sec555-wiki -v /scripts:/scripts -v /var/run/docker.sock:/var/run/docker.sock hasecuritysolutions/wikiup"
