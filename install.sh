21b0efce3b870ba8454cf822abd186265e5712b2
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-20 16:58:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,21 +45,20 @@ xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/works
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/image-style --set 1
 
 # Pull docker images
-sudo docker pull hasecuritysolutions/wikiup
 sudo docker pull httpd
 sudo docker run -d --name wiki --restart always --net=bridge -p 80:80 -v /var/www/sec555-wiki:/usr/local/apache2/htdocs/ httpd
 sudo docker pull rabbitmq
 sudo docker run -d --name rabbitmq --hostname rabbitmq --net=bridge -p 8080:8080 -p 15672:15672 -e RABBITMQ_VM_MEMORY_HIGH_WATERMARK=0.25 -e RABBITMQ_DEFAULT_USER=student -e RABBITMQ_DEFAULT_PASS=sec555 -v /labs/rabbitmq/mnesia:/var/lib/rabbitmq/mnesia rabbitmq:3-management
-sudo docker pull hasecuritysolutions/wikiup
-sudo docker pull hasecuritysolutions/elastic_cron
-sudo docker pull hasecuritysolutions/elastalert
+sudo docker pull hasecuritysolutions/wikiup:v1.0
+sudo docker pull hasecuritysolutions/elastic_cron:v1.0
+sudo docker pull hasecuritysolutions/elastalert:v0.1.31
 sudo docker pull docker.elastic.co/beats/filebeat:6.2.4
-sudo docker pull hasecuritysolutions/logstashoss
+sudo docker pull hasecuritysolutions/logstashoss:v6.2.2
 
 sudo chmod 755 /labs/1.1/filebeat.yml
 alias filebeat="docker run -it --rm --net=bridge --name filebeat --hostname filebeat -v /labs:/labs:ro -v /var/log:/var/log:ro --link logstash docker.elastic.co/beats/filebeat:6.2.4 /usr/share/filebeat/filebeat"
-alias logstash="docker run -it --rm --net=bridge --name logstash --hostname logstash -v /labs:/labs --link elasticsearch --link freq_server --link domain_stats -e ELASTICSEARCH_HOST=elasticsearch -p 5044:5044 -p 5045:5045 -p 6000:6000 -p 6050:6050 hasecuritysolutions/logstashoss /usr/share/logstash/bin/logstash"
-alias pwsh="docker run -it -v /labs:/labs -v /scripts:/scripts -v /var/www:/var/www -v /home/student:/home/student -v /var/run/docker.sock:/var/run/docker.sock --rm --link elasticsearch hasecuritysolutions/wikiup /usr/bin/pwsh"
-alias curator="docker run -it -v /etc/localtime:/etc/localtime:ro --name curator --hostname curator -v /labs/curator:/labs/curator:ro -e TZ=America/Chicago -e ELASTICSEARCH_HOST=elasticsearch --net=bridge --rm --link elasticsearch hasecuritysolutions/elastic_cron /usr/local/bin/curator"
-alias elastalert="docker run -it --rm --net=bridge --name elastalert --hostname elastalert -v /labs/elastalert:/labs/elastalert --link elasticsearch hasecuritysolutions/elastalert /usr/local/bin/elastalert"
-alias wikiup="docker run -it --rm --net=bridge -v /labs:/labs -v /var/www/sec555-wiki:/var/www/sec555-wiki -v /scripts:/scripts -v /var/run/docker.sock:/var/run/docker.sock hasecuritysolutions/wikiup"
+alias logstash="docker run -it --rm --net=bridge --name logstash --hostname logstash -v /labs:/labs --link elasticsearch --link freq_server --link domain_stats -e ELASTICSEARCH_HOST=elasticsearch -p 5044:5044 -p 5045:5045 -p 6000:6000 -p 6050:6050 hasecuritysolutions/logstashoss:v6.2.2 /usr/share/logstash/bin/logstash"
+alias pwsh="docker run -it -v /labs:/labs -v /scripts:/scripts -v /var/www:/var/www -v /home/student:/home/student -v /var/run/docker.sock:/var/run/docker.sock --rm --link elasticsearch hasecuritysolutions/wikiup:v1.0 /usr/bin/pwsh"
+alias curator="docker run -it -v /etc/localtime:/etc/localtime:ro --name curator --hostname curator -v /labs/curator:/labs/curator:ro -e TZ=America/Chicago -e ELASTICSEARCH_HOST=elasticsearch --net=bridge --rm --link elasticsearch hasecuritysolutions/elastic_cron:v1.0 /usr/local/bin/curator"
+alias elastalert="docker run -it --rm --net=bridge --name elastalert --hostname elastalert -v /labs/elastalert:/labs/elastalert --link elasticsearch hasecuritysolutions/elastalert:v0.1.31 /usr/local/bin/elastalert"
+alias wikiup="docker run -it --rm --net=bridge -v /labs:/labs -v /var/www/sec555-wiki:/var/www/sec555-wiki -v /scripts:/scripts -v /var/run/docker.sock:/var/run/docker.sock hasecuritysolutions/wikiup:v1.0"
